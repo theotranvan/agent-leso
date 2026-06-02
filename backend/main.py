860677d73f501a034d9d9be0548a0e50f86d155e
theotranvan <theocours34@gmail.com>
2026-06-02 17:23:46 +0200

    Change CORS settings to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -92,7 +92,7 @@ app = FastAPI(
 # CORS restrictif
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type", "Stripe-Signature"],
